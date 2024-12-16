d414f765ff6e63d954d1963cc66535acb3fee9ae
adolescent <39371189+adolescent@users.noreply.github.com>
2024-12-16 18:54:04 +0800

    Update Fill_Nan.py

--- _Projects/241216_Nan_Filler/Fill_Nan.py
+++ _Projects/241216_Nan_Filler/Fill_Nan.py
@@ -14,6 +14,10 @@ import seaborn as sns
 
 wp=r'D:\ZR\_Data_Temp\Ois200_Data\red_speckle'
 #%% Generate 
-Single_Folder_Processor(wp,subfolder='Preprocessed',save_format='python')
+Single_Folder_Processor(wp,subfolder='Preprocessed',save_format='python',keepna=True)
 #%% let's see the frame drop problems here.
 a = np.load(r'D:\ZR\_Data_Temp\Ois200_Data\red_speckle\Preprocessed\red.npy')
+b = np.load(r'D:\ZR\_Data_Temp\Ois200_Data\red_speckle\Preprocessed\Speckle.npy')
+
+plt.plot(a.sum(-1).sum(-1))
+plt.plot(b.sum(-1).sum(-1))
