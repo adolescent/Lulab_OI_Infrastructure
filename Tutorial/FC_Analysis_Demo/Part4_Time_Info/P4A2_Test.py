046837c33a375514934451352a843b767dd67ed3
adolescent <zhang938336689@gmail.com>
2025-02-10 10:56:49 +0800

    HMM test done

--- Tutorial/FC_Analysis_Demo/Part4_Time_Info/P4A2_Test.py
+++ Tutorial/FC_Analysis_Demo/Part4_Time_Info/P4A2_Test.py
@@ -21,7 +21,7 @@ import seaborn as sns
 import umap
 import copy
   
-wp = r'D:\ZR\_Data_Temp\Ois200_Data\Full_Demo\Wild_Type\Preprocessed'
+wp = r'D:\_DataTemp\OIS\Wild_Type\Preprocessed'
 MG = Mask_Generator(bin=4)
 
 
@@ -48,7 +48,7 @@ from PIL import Image  # For GIF processing
 # u = coords_raw[:,[0,1,2]] # for spatial PCA
 # u = PC_Comp_raw_t[[0,1,2],:]  # for temporal PCA
 start = 10000
-end = 11500
+end = 13000
 plotable = u[start:end,:]
 
 
@@ -94,36 +94,88 @@ X = data.T  # Reshape to (18000, 1400)
 
 # Standardize features (recommended)
 scaler = StandardScaler() # z score
-X_scaled = scaler.fit_transform(X)
+X_scaled = np.clip(scaler.fit_transform(X),-3,3)
+# X_scaled = X_scaled/3
 #
 import os
 os.environ["OMP_NUM_THREADS"] = '1.'
 # Initialize the HMM
-# model = hmm.GaussianHMM(n_components=4, covariance_type="diag")
-model = hmm.GMMHMM(n_components=7, covariance_type="diag")
+model = hmm.GaussianHMM(n_components=12, covariance_type="diag",n_iter = 1000,tol=0.001)
+# model = hmm.GMMHMM(n_components=15, covariance_type="diag",n_iter = 10000,tol=0.0001)
 
 # Fit the model
 model.fit(X_scaled)
 # Predict the hidden states for each time step
 hidden_states = model.predict(X_scaled)
+# a = model.predict_proba(X_scaled)
 cf.Save_Variable(wp,'HMM_Model',model)
 # hidden_states.shape = (18000,)
 #%% show hidden state
 # Mean vectors (shape: 6x1400)
 model = cf.Load_Variable(wp,'HMM_Model.pkl')
-all_mats = model.means_[:,0,:]
-
+all_mats = model.means_
+# all_mats = model.means_[:,0,:] # for GMMHMM
 # Covariance matrices (shape: 6x1400)
 state_covariance_matrices = model.covars_
 
 # Transition matrix (shape: 6x6)
 transition_matrix = model.transmat_
+# sns.heatmap(transition_matrix)
 #%% test recover
 # c_vec =on_response[:,hidden_states==3].mean(1)
-c_vec = all_mats[6,:]
+c_vec = all_mats[4,:]
 # c_vec = state_covariance_matrices[6,0,:]
 
 recover = copy.deepcopy(on_mask).astype('f8')
 recover[recover==True] = c_vec
 sns.heatmap(recover,square=True)
 # sns.heatmap(recover,square=True,center = -0.6,vmin=-0.9,vmax = -0.3)
+
+
+#%%
+# sample new data from this hmm.
+X, Z = model.sample(50)
+recover = copy.deepcopy(on_mask).astype('f8')
+recover[recover==True] = X[0,:]
+sns.heatmap(recover,square=True)
+#%%
+# well , it seems not good enough. Let's try something different.
+# area_resp = np.zeros(shape = ())
+series = np.load(cf.join(wp,'z_series.npy'))
+mask = cv2.imread(cf.join(wp,'Chamber_mask.png'),0)>0
+mask = mask*mask[:,::-1]
+joint_mask = (series.std(0)>0)*mask
+series = series*joint_mask
+
+# a = MG.Avr_By_Area(series[0,:,:])
+area_resp = np.zeros(shape = (1802,42),dtype = 'f8')
+for i in tqdm(range(1802)):
+    area_resp[i,:] = np.array(MG.Avr_By_Area(series[i,:,:])['Response'])
+
+# del series
+#%%
+scaler = StandardScaler() # z score
+X_scaled = np.clip(scaler.fit_transform(area_resp),-3,3)
+# X_scaled = X_scaled/3
+#
+import os
+os.environ["OMP_NUM_THREADS"] = '1.'
+# Initialize the HMM
+model = hmm.GaussianHMM(n_components=7, covariance_type="diag",n_iter = 10000,tol=0.001)
+# model = hmm.GMMHMM(n_components=15, covariance_type="diag",n_iter = 10000,tol=0.0001)
+
+# Fit the model
+model.fit(X_scaled)
+# Predict the hidden states for each time step
+hidden_states = model.predict(X_scaled)
+all_mats = model.means_
+
+#%%
+a = MG.Avr_By_Area(series[0,:,:])
+X, Z = model.sample(150)
+sns.heatmap(X)
+
+#%% plot recovered graph
+# c_map = MG.Get_Weight_Map(a['Name'],X[0,:])
+c_map = MG.Get_Weight_Map(a['Name'],all_mats[4,:])
+sns.heatmap(c_map,square=True,center=0)
